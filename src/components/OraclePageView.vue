fa9a89ef420fcc76e9e311df66c3594b23ab614b
Piti Ongmongkolkul <piti118@gmail.com>
2023-03-03 17:01:10 +0700

    update

---
+++ src/components/OraclePageView.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <div>
+    <QuestionFormVue text="question" @change="onTextChange"/>
+    <AskButton/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import AskButton from './AskButton.vue';
+import type QuestionFormVue from './QuestionForm.vue';
+
+const question = ref('')
+
+function onTextChange(event: Event){
+  console.log(event)
+  question.value = event.target!.value
+}
+</script>
---
+++ src/components/QuestionForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <input 
+  type="text" 
+  value="text" 
+  @change="$emit('change', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps<{text: string}>()
+</script>
